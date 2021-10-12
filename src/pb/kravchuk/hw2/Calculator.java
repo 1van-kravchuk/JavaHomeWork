00e28699868547025eb0d12b4264c23b3d26636d
vick <ivan.kravchuk01@gmail.com>
2021-10-12 22:28:45 +0300

    hw1

--- src/pb/kravchuk/hw2/Calculator.java
+++ src/pb/kravchuk/hw2/Calculator.java
@@ -4,29 +4,29 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        int result = 0;
+        double result = 0;
         Scanner scan = new Scanner(System.in);
-        System.out.println("введите число а:");
-        int a = scan.nextInt();
-        System.out.println("введите число b:");
-        int b = scan.nextInt();
+        System.out.println("введите первое число:");
+        int operand1 = scan.nextInt();
+        System.out.println("введите второе число:");
+        int operand2 = scan.nextInt();
         System.out.println("введите знак действия:");
         String sign = scan.next();
-        if (b == 0 && sign.equals("/")){
+        if (operand2 == 0 && sign.equals("/")){
             System.out.println("Делить на 0 нельзя");
         } else {
             switch (sign){
                 case "+":
-                    result = a+b;
+                    result = operand1 + operand2;
                     break;
                 case "-":
-                    result = a-b;
+                    result = operand1 - operand2;
                     break;
                 case "*":
-                    result = a*b;
+                    result = operand1 * operand2;
                     break;
                 case "/":
-                    result = a/b;
+                    result = ((double)operand1) / operand2;
                     break;
             }
             
